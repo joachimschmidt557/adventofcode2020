a9cc754ee8888b391e77100cc2b4af5788c44175
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-11-30 23:10:56 +0100

    Add build.zig and gitignore

---
+++ build.zig
@@ -0,0 +1,31 @@
+const Builder = @import("std").build.Builder;
+
+const days = [_][]const u8{};
+
+pub fn build(b: *Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    inline for (days) |day| {
+        const exe = b.addExecutable("adventofcode2020", "src/" + day + ".zig");
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
+        exe.install();
+
+        const run_cmd = exe.run();
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run " ++ day);
+        run_step.dependOn(&run_cmd.step);
+    }
+}
